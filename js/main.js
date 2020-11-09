561f32bb6b08c0edf855d9e4378687870c963400
Andrea <andre98377@gmail.com>
2020-11-09 15:24:30 +0100

    vue-slider

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Main js
+const app = new Vue({
+    el: '#app',
+    data: {
+        indexPhoto: 0,
+        photos: [
+            './img/immage1.jpg',
+            './img/immage2.jpg',
+            './img/immage3.jpg',
+            './img/immage4.jpg',
+        ],
+
+    }
+});
